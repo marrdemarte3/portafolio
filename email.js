843866e1496485c1bb48e2552f7809d23adc2757
Marcela Franco <marcelafrancoal1997@gmail.com>
2024-08-30 18:56:09 -0600

    cambios
    ultimos cambios

--- email.js
+++ email.js
@@ -1,20 +1,20 @@
 const btn = document.getElementById('button');
 
 document.getElementById('form')
-    .addEventListener('submit', function (event) {
-        event.preventDefault();
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
 
-        btn.value = 'Sending...';
+   btn.value = 'Sending...';
 
-        const serviceID = 'default_service';
-        const templateID = 'template_w1fz9gr';
+   const serviceID = 'default_service';
+   const templateID = 'template_w1fz9gr';
 
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
-                btn.value = 'Send Email';
-                alert('Sent!');
-            }, (err) => {
-                btn.value = 'Send Email';
-                alert(JSON.stringify(err));
-            });
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
     });
+});
